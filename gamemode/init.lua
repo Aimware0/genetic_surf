fe031930b5c56ad487695f53bc97e50f5635d1d1
Riddle <>
2022-05-28 09:37:57 -0400

    Added no collision & kill_command

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -4,6 +4,17 @@ AddCSLuaFile( "shared.lua" )
 include( "shared.lua" )
 
 
+function GM:Initialize()
+
+end
+
+function GM:PlayerSpawn(ply)
+    ply:SetModel( "models/player/putin.mdl" )
+    ply:SetCollisionGroup(10)
+    ply:SetNoCollideWithTeammates(true)
+
+end
+
 
 hook.Add("PlayerSay", "PlayerChat", function(ply, text, team)
     if text == "!r" or text == "!restart" then
@@ -14,7 +25,13 @@ hook.Add("PlayerSay", "PlayerChat", function(ply, text, team)
 end)
 
 
-
+concommand.Add("kill_add", function()
+    for k, v in pairs(player.GetAll()) do
+        if v:Nick() ~= "x" then
+            v:Kill()
+        end
+    end
+end)
 
 
 
